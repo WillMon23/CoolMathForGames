c57fc5be10738ca89af58c2ac8ff10e6a27b40b5
Will <montero_w@live.com>
2021-10-06 16:55:37 -0500

    Created Template For Cool Game For Math

---
+++ CoolMathForGames/Engine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolMathForGames
+{
+    class Engine
+    {
+
+        private static bool _applicationShouldClose = false;
+        private static int _currentSceneIndex;
+        private Scene[] _scenes = new Scene[0];
+
+
+        /// <summary>
+        /// Called to begin the application 
+        /// </summary>
+       public void Run()
+        {
+            // Call start for the entire application 
+            Start();
+
+            // Loop until the application is told to close
+            while(!_applicationShouldClose)
+            {
+                Draw();
+                Update();
+            }
+            // Called end for the entire application
+            End();
+        }
+
+        /// <summary>
+        /// Called when application starts 
+        /// </summary>
+        private void Start()
+        {
+            _scenes[_currentSceneIndex].Update();
+        }
+
+        /// <summary>
+        /// Called to draw to the scene 
+        /// </summary>
+        private void Draw()
+        {
+            _scenes[_currentSceneIndex].Draw();
+        }
+
+        /// <summary>
+        /// Updates the application and notifies the console of any changes 
+        /// </summary>
+        private void Update()
+        {
+            _scenes[_currentSceneIndex].Update();
+        }
+
+        /// <summary>
+        /// Called once the game has been set to game over 
+        /// </summary>
+        private void End()
+        {
+            _scenes[_currentSceneIndex].End();
+        }
+
+        /// <summary>
+        /// Created to append new scnene to the current listing of scene 
+        /// </summary>
+        /// <param name="scene">Scene being added to the current list of scens</param>
+        /// <returns>returns the new ammount of scenes</returns>
+        public int AddScene(Scene scene)
+        {
+            // Creats a Temporary array 
+            Scene[] tempArray = new Scene[_scenes.Length + 1];
+
+            //Copys all the values from old array info to the temp array
+            for (int i = 0; i < _scenes.Length; i++)
+                tempArray[i] = _scenes[i];
+            //Sets adds the new scene to the new size
+            tempArray[_scenes.Length] = scene;
+            // Set the old array to the new array
+            _scenes = tempArray;
+            // returns the new allocated size
+            return _scenes.Length - 1;
+        }
+    }
+}
---
+++ CoolMathForGames/Scene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolMathForGames
+{
+    class Scene
+    {
+        public Scene()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Draw()
+        {
+
+        }
+        public void End()
+        {
+            
+        }
+    }
+}
